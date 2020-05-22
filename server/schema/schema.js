c62d1b2c709d8fb1d7583f875255285310ddc33c
sabujhasansarker <sabujhasansarker@gmail.com>
2020-05-22 13:34:58 +0600

    #19 More on Mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -93,6 +93,22 @@ const Mutation = new GraphQLObjectType({
         // * author.save() only save data and {return} use for query data
       },
     },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId,
+        });
+        return book.save();
+      },
+    },
   },
 });
 
