38c1cef174e80ae4abec1d7498a986bcf50563a1
sabujhasansarker <sabujhasansarker@gmail.com>
2020-05-22 19:33:32 +0600

    #25  Apollo Client Setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,23 @@ import React, { Component } from "react";
 // Components
 import Booklist from "./components/BookList";
 
+// Appllo GraphQL
+import AppolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+// Apollo client setup
+const client = new AppolloClient({
+  uri: `http://localhost:4000/graphql`,
+});
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Ninja's Reading List</h1>
-        <Booklist />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <Booklist />
+        </div>
+      </ApolloProvider>
     );
   }
 }
