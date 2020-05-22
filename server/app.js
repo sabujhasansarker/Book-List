4d66feb8a4cea4099d3ff52c12ae7d0d22906b22
sabujhasansarker <sabujhasansarker@gmail.com>
2020-05-22 12:42:22 +0600

    #1-15 GraphQL setup

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is runing on port ${PORT}`);
+});
